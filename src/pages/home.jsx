11b421169cbbc64513131750efb7edc206475172
Griffin Hampton <gmhamp01@louisville.edu>
2026-03-25 00:09:27 -0400

    svg/layout

---
+++ src/pages/home.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import '../App.css'
+
+function Home() {
+  console.log("bitch");
+  return (
+    <>
+    <body>
+      <h1>Hello, world</h1>
+    </body>
+    </>
+  )
+}
+
+export default Home
---
+++ src/pages/login.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import '../App.css'
+
+function Login() {
+  console.log("bitch");
+  return (
+    <>
+    <body>
+      <h1>Goodbye, world</h1>
+    </body>
+    </>
+  )
+}
+
+export default Login
